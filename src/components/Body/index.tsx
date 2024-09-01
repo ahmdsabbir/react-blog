20dbe9a4790c9b526419d44e113af3a4d32fdb05
ahmdsabbir <ahmd.sabbir@gmail.com>
2024-09-01 17:03:00 +0600

    feat: basic app

---
+++ src/components/Body/index.tsx
@@ -0,0 +1,10 @@
+function Body() {
+
+  return (
+    <main>
+      <h2>Body</h2>
+    </main>
+  );
+}
+
+export default Body;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+function Header() {
+  return(
+    <header>
+      <h1>Header</h1>
+      <nav>
+        <ul>
+          <li><a href="#">home</a></li>
+          <li><a href="#">blog</a></li>
+          <li><a href="#">about</a></li>
+          <li><a href="#">contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
